0a02028fab5ac73c1877ee053774af662b503cac
Matheus Silva Tavares <matheus.tavares@cencosud.com.br>
2022-05-03 16:11:12 -0300

    initial commit

---
+++ src/pages/Feira/NavBar/index.js
@@ -0,0 +1,20 @@
+import { Nav } from './styles';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import { useHistory } from "react-router-dom";
+
+export default function NavBar() {
+  const history = useHistory();
+  return (
+    <Nav>
+      <Logo />
+      <IconButton onClick={() => history.push("/carrinho")}>
+        <Badge color="primary">
+          <ShoppingCartIcon />
+        </Badge>
+      </IconButton>
+    </Nav>
+  );
+}
